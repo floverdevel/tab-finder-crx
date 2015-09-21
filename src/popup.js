e51547a1342b6fa959172994e4bc03f71ce08b56
Ellis Antaya <eantaya@lapresse.ca>
2015-09-21 12:41:42 -0400

    refactoring function scope, external css file

--- src/popup.js
+++ src/popup.js
@@ -4,73 +4,21 @@
 (function (window, chrome) {
     'use strict';
 
-    let currentSelectedDisplayedTab = -1;
-    const KEY_ENTER = 13;
-    const KEY_UP = 38;
-    const KEY_DOWN = 40;
+    console.log('User agent is %o', window.navigator.userAgent);
 
     chrome.tabs.query({}, function (tabs) {
 
+        var currentSelectedDisplayedTab = -1;
+        const KEY_ENTER = 13;
+        const KEY_UP = 38;
+        const KEY_DOWN = 40;
+
         for (let i = 0; i < tabs.length; i += 1) {
-            let tab = tabs[i];
-            let a = window.document.createElement('a');
-            let p = window.document.createElement('p');
-            let img = window.document.createElement('img');
-            let em = window.document.createElement('em');
-            let br = window.document.createElement('br');
-
-
-            a.textContent = tab.title;
-            a.title = tab.url;
-            a.href = tab.url;
-            a.onclick = function () {
-                return false;
-            };
-
-            if (tab.url.indexOf('chrome://') == 0) {
-                img.src = 'resources/crx-favicon.png';
-            } else if (tab.url.indexOf('file://') == 0) {
-                img.src = 'resources/file.png';
-            } else {
-                img.src = tab.favIconUrl;
-            }
-            img.onerror = function () {
-                this.src = 'resources/chrome-32.png';
-            };
-
-            em.textContent = tab.url;
-
-            p.classList.add('visible');
-            p.title = tab.url;
-            p.setAttribute('data-tab-window-id', tab.windowId);
-            p.setAttribute('data-tab-id', tab.id);
-            p.onclick = function () {
-                let tabWindowId = parseInt(this.attributes['data-tab-window-id'].value);
-                let tabId = parseInt(this.attributes['data-tab-id'].value);
-
-                chrome.tabs.update(tabId, {
-                    'active': true
-                }, function () {
-                    chrome.windows.update(tabWindowId, {
-                        'focused': true
-                    });
-                });
-            };
-            p.onmouseover = function () {
-                selectTab(this);
-            };
-            p.onmouseout = function () {
-                // unselectAllTabs();
-            };
-
-            p.appendChild(img);
-            p.appendChild(a);
-            p.appendChild(br);
-            p.appendChild(em);
-            window.document.body.appendChild(p);
+            window.document.body.getElementsByTagName('ul')[0].appendChild(createMenuItemFromTab(tabs[i]));
         }
 
-        window.addEventListener('keydown', function (event) {
+        var searchInput = window.document.getElementById('searchInput');
+        searchInput.addEventListener('keydown', function (event) {
 
             switch (event.which) {
                 case KEY_ENTER : {
@@ -101,9 +49,8 @@
             selectTab(displayedTabs[currentSelectedDisplayedTab]);
         });
 
-        var searchInput = window.document.getElementById('searchInput');
         searchInput.addEventListener('input', function (event) {
-            let displayedTabs = window.document.getElementsByTagName('p');
+            let displayedTabs = window.document.getElementsByTagName('li');
             for (let i = 0; i < displayedTabs.length; i += 1) {
                 let displayedTab = displayedTabs[i];
                 if (this.value == '') {
@@ -114,48 +61,111 @@
                     let textPosition = displayedTab.textContent.toLowerCase().indexOf(this.value.toLowerCase());
                     if (textPosition != -1) {
                         displayedTab.classList.add('visible');
-                        displayedTab.classList.remove('hidden');
                     } else {
                         displayedTab.classList.remove('visible');
-                        displayedTab.classList.add('hidden');
                     }
                     highlightTextInElement(this.value, displayedTab.getElementsByTagName('a')[0]);
                     highlightTextInElement(this.value, displayedTab.getElementsByTagName('em')[0]);
                 }
             }
         });
+
+        function highlightTextInElement(text, element) {
+            let textPosition = element.textContent.toLowerCase().indexOf(text.toLowerCase());
+            if (textPosition != -1) {
+                element.innerHTML = element.textContent.substr(0, textPosition) +
+                    '<span class="highlight">' +
+                    element.textContent.substr(textPosition, text.length) +
+                    '</span>' +
+                    element.textContent.substr(textPosition + text.length);
+            } else {
+                element.innerHTML = element.textContent
+            }
+
+        }
+
+        function unselectAllTabs() {
+            let displayedTabs = window.document.getElementsByTagName('li');
+            for (let i = 0; i < displayedTabs.length; i += 1) {
+                let displayedTab = displayedTabs[i];
+                displayedTab.classList.remove('selected');
+            }
+        }
+
+        function selectTab(tab) {
+            unselectAllTabs();
+            tab.classList.add('selected');
+            //currentSelectedDisplayedTab = tab;
+        }
+
+        function createMenuItemFromTab(tab) {
+            var li = window.document.createElement('li');
+            li.classList.add('visible');
+            li.title = tab.url;
+            li.setAttribute('data-tab-window-id', tab.windowId);
+            li.setAttribute('data-tab-id', tab.id);
+            li.addEventListener('click', function () {
+                let tabWindowId = parseInt(this.attributes['data-tab-window-id'].value);
+                let tabId = parseInt(this.attributes['data-tab-id'].value);
+
+                chrome.tabs.update(tabId, {
+                    'active': true
+                }, function () {
+                    chrome.windows.update(tabWindowId, {
+                        'focused': true
+                    });
+                });
+            });
+            li.addEventListener('mouseover', function () {
+                selectTab(this);
+            });
+
+            li.appendChild(createFavIconFromTab(tab));
+            li.appendChild(createHyperLinkFromTab(tab));
+            li.appendChild(window.document.createElement('br'));
+            li.appendChild(createUrlFromTab(tab));
+            return li;
+
+            function createFavIconFromTab(tab) {
+                var element = window.document.createElement('img');
+                element.classList.add('small');
+                if (tab.url.indexOf('chrome://') == 0) {
+                    element.src = 'resources/crx-favicon.png';
+                } else if (tab.url.indexOf('file://') == 0) {
+                    element.src = 'resources/file.png';
+                } else {
+                    element.src = tab.favIconUrl;
+                }
+                element.onerror = function () {
+                    this.src = getFavIconBasedOnPlatform();
+                };
+
+                return element;
+            }
+
+            function getFavIconBasedOnPlatform () {
+                return 'resources/chrome-32.png';
+            }
+
+            function createHyperLinkFromTab(tab) {
+                var element = window.document.createElement('a');
+                element.textContent = tab.title;
+                element.title = tab.url;
+                element.href = tab.url;
+                element.onclick = function (event) {
+                    event.preventDefault();
+                };
+
+                return element;
+            }
+
+            function createUrlFromTab(tab) {
+                var element = window.document.createElement('em');
+                element.textContent = tab.url;
+
+                return element;
+            }
+        }
     });
-
-    function highlightTextInElement(text, element) {
-        let textPosition = element.textContent.toLowerCase().indexOf(text.toLowerCase());
-        if (textPosition != -1) {
-            let title = element.textContent;
-            let highlightedContent =
-                element.textContent.substr(0, textPosition) +
-                '<span class="highlight">' +
-                element.textContent.substr(textPosition, text.length) +
-                '</span>' +
-                element.textContent.substr(textPosition + text.length);
-            element.innerHTML = highlightedContent;
-        } else {
-            element.innerHTML = element.textContent
-        }
-
-    }
-
-    function unselectAllTabs() {
-        let displayedTabs = window.document.getElementsByTagName('p');
-        for (let i = 0; i < displayedTabs.length; i += 1) {
-            let displayedTab = displayedTabs[i];
-            displayedTab.classList.remove('selected');
-        }
-    }
-
-    function selectTab(tab) {
-        unselectAllTabs();
-        tab.classList.add('selected');
-        //currentSelectedDisplayedTab = tab;
-    }
-
 
 })(window, chrome);
